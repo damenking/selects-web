849b05dae84059ee8fb11f0f700ab5f8399b8dc5
Damen King <damen.king@gmail.com>
2020-05-01 22:58:17 -0700

    added rough orders and favorites sections from from pages to componenets

--- client/components/account/Favorites.tsx
+++ client/components/account/Favorites.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { getProductsByIds } from '../../api/shopify/products';
+import UserContext from '../../components/UserContext';
+import { ProductFavorite } from '../../interfaces/';
 
 // import styles from './Favorites.module.css';
 
 const Favorites: React.FunctionComponent = () => {
+  const { favorites } = useContext(UserContext);
+  const [products, updateProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getProductsByIds(favorites.product);
+      updateProducts(response.products);
+    };
+    if (favorites.product.length) {
+      fetchData();
+    }
+  }, [favorites]);
   return (
     <div>
       <h4>Favorites Page</h4>
+      {products.map((product: ProductFavorite, index: number) => {
+        return (
+          <div key={index}>
+            <p>{product.title}</p>
+            <img src={product.image.src} height="100" width="100" />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- client/components/account/Orders.tsx
+++ client/components/account/Orders.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCustomerOrders } from '../../api/shopify/orders';
 
 // import styles from './Orders.module.css';
 
 const Orders: React.FunctionComponent = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = localStorage.getItem('accessToken');
+      if (token) {
+        const { orders } = await getCustomerOrders(token);
+        setOrders(orders);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h4>Orders Page</h4>
+      {orders.map((order: any, index: number) => {
+        return (
+          <div key={index}>
+            <h5>Order #{order.orderNumber}</h5>
+            <div>
+              <span>Create at: {order.createdAt}</span>
+              {order.lineItems.map((lineItem: any, index: number) => {
+                return (
+                  <div key={index}>
+                    <ul>
+                      <li>Product: {lineItem.title}</li>
+                      <li>Quantity: {lineItem.quantity}</li>
+                      <li>Duration: {lineItem.variant_title}</li>
+                      <li>Price: ${lineItem.price}</li>
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
